4ff642bdf0ae2b9f3344f6d4559b4a0c48657dc0
Marc R. Uchniat <logan5@feem.net>
2020-06-19 11:19:01 -0500

    add internet connection monitor with led output

---
+++ include/wificonfig.h
@@ -0,0 +1,3 @@
+#define WIFI_SSID           "MY_SSID"
+#define WIFI_PASSWORD       "MY_PASSWORD"
+#define PING_HOST           "1.1.1.1
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,67 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
+#include "wificonfig.h"
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+const char* remote_host = PING_HOST;
+
+const int RED_LED = D0;
+const int BLUE_LED = D4;
+
+bool last_ping = false;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    delay(10);
+
+    pinMode(RED_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+
+    Serial.println();
+    Serial.println("Connecting");
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+
+    Serial.println("Connected!");
+    Serial.println();
+    Serial.print("screamiboi is ");
+    Serial.print(WiFi.localIP());
+    Serial.println(". ALL SHALL LOVE ME AND DESPAIR!");
+
+    Serial.print("Pinging host ");
+    Serial.print(remote_host);
+    Serial.println("...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if(Ping.ping(remote_host)) {
+        if(last_ping != true) {
+            Serial.println("Connected!");
+            Serial.print("Monitoring connection");
+        } else {
+            Serial.print(".");
+        }
+        last_ping = true;
+        digitalWrite(BLUE_LED, LOW);
+        digitalWrite(RED_LED, HIGH);
+    } else {
+        if(last_ping == true) {
+            Serial.println("Connection lost!");
+            Serial.print("Waiting for recovery");
+        } else {
+            Serial.print(".");
+        }
+        last_ping = false;
+        digitalWrite(BLUE_LED, HIGH);
+        digitalWrite(RED_LED, LOW);
+    }
+    delay(500);
 }
