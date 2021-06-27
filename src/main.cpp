2217e44f185e8b00c1996d60a90cd41ca1986d7b
Marc R. Uchniat <logan5@feem.net>
2021-06-27 11:48:17 -0500

    add display control, button scaffold (needs api)

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
+#include <AceButton.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <AsyncPing.h>
 #include <Ticker.h>
+#include <U8g2lib.h>
+#include <Wire.h>
 #include "wificonfig.h"
+using namespace ace_button;
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// uncomment to debug
+#define SCREAM_DEBUG
+
+// init I2C SSD1306 4-wire display
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   
+byte displayCount = 0;
+
+// button configs
+ButtonConfig rebootConfig;
+
+// buttons
+AceButton rebootButton(&rebootConfig);
+
+// time me
+Ticker timer;
+
+// ping me
+AsyncPing pings[3];
+IPAddress addrs[3];
+
+// wifi
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 
+// whomst shall i pingst
 const char *hosts[3] = {PING_HOSTS};
 
-const int RED_LED = D7; // add an external led
-const int YELLOW_LED = D8;
-const int LEDS[4] = { D0, D4, D7, D8 };
-const int LEDS_OFF[2] = { D0, D4 };
+// pins
+const int RESET_PIN = D6;
+const int buzzer = D5;
 
+// globals
 
 bool last_ping[3] = { true, true, true };
 bool net_fail = false;
@@ -21,9 +50,37 @@ bool net_fail = false;
 int win_count = 0;
 int lose_count = 0;
 
-Ticker timer;
-AsyncPing pings[3];
-IPAddress addrs[3];
+// prototypes
+void displayClear();
+void displaySad();
+void handleRebootEvent(AceButton*, uint8_t, uint8_t);
+void pingu();
+
+void displayClear() {
+    display.clearBuffer();
+    display.sendBuffer();
+}
+
+void displaySad() {
+    display.clearBuffer();
+    display.setFont(u8g2_font_open_iconic_www_4x_t);
+    display.drawGlyph(21, 50, 0x51);
+    display.drawGlyph(67, 50, 0x4a);
+    display.sendBuffer();
+}
+
+void handleRebootEvent(AceButton*, uint8_t eventType, uint8_t) {
+    switch (eventType) {
+        case AceButton::kEventLongPressed:
+            tone(buzzer, 500, 100);
+            delay(150);
+            tone(buzzer, 500, 100);
+            delay(150);
+            tone(buzzer, 500, 300);
+            delay(300);
+            break;
+    }
+}
 
 void pingu() {
     bool allFalse = false;
@@ -50,25 +107,19 @@ void pingu() {
 
     if ( net_fail ) {
         if ( win_count >= 3 ) {
-            Serial.printf("\nConnection up! Monitoring...");
-            digitalWrite(RED_LED, LOW);
-            digitalWrite(YELLOW_LED, LOW);
             net_fail = false;
+            displayClear();
         }
     } else {
         if ( lose_count ) {
             if (allFalse) {
                 if ( lose_count >= 18) {
-                    Serial.printf("\nConnection down! Monitoring...");
-                    digitalWrite(YELLOW_LED, LOW);
-                    digitalWrite(RED_LED, HIGH);
                     net_fail = true;
+                    displaySad();
                 } else {
-                    digitalWrite(YELLOW_LED, HIGH);
                 }
             }
         } else {
-            digitalWrite(YELLOW_LED, LOW);
         }
     }
     for (int i = 0; i < 3; i++) {
@@ -77,29 +128,22 @@ void pingu() {
 }
 
 void setup() {
-    Serial.begin(115200);
-    delay(10);
+    // give me your face
+    display.begin();
+    displayClear();
+    
+    // PUSH ME
+    rebootConfig.setEventHandler(handleRebootEvent);
+    rebootConfig.setFeature(ButtonConfig::kFeatureLongPress);
+    pinMode(RESET_PIN, INPUT);
+    rebootButton.init(RESET_PIN, LOW);
 
-    for (int i: LEDS) {
-        pinMode(i, OUTPUT);
-        digitalWrite(i, HIGH);
-    }
+    // buzz buzz motherfucker
+    pinMode(buzzer, OUTPUT);
 
-    Serial.printf("\nConnecting...");
-
+    // let's get going
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(100);
-        Serial.print(".");
-    }
-
-    Serial.printf("Connected!\nscreamiboi is %s. ALL SHALL LOVE ME AND DESPAIR!\n",
-                  WiFi.localIP().toString().c_str());
-    Serial.printf("Pinging hosts...");
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(YELLOW_LED, LOW);
 
     for (int i = 0; i < 3; i++) {
         if (hosts[i]) {
@@ -108,10 +152,8 @@ void setup() {
         }
         pings[i].on(true, [i](const AsyncPingResponse& response) {
             if (response.answer) {
-                Serial.printf("!");
                 last_ping[i] = true;
             } else {
-                Serial.printf(".");
                 last_ping[i] = false;
             }
             return false;
@@ -122,4 +164,8 @@ void setup() {
     }
 }
 
-void loop() {}
+void loop() {
+    if ( net_fail ) {
+       rebootButton.check();
+    }
+}
